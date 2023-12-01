69852f66ccff0575afb091869494b145811a6829
FairyLightLux <chrononcross@gmail.com>
2023-11-30 20:38:37 -0500

    Created 9 rooms
    Some room transitions still need to be adjusted

--- Assets/Scripts/DoorCameraController.cs
+++ Assets/Scripts/DoorCameraController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class DoorCameraController : MonoBehaviour
 {
-    public Camera thisCamera;
-    public Camera newCamera;
+    public GameObject thisCameraObj;
+    public GameObject newCameraObj;
     public Transform newPositionTarget;
     private Vector3 newPosition;
+    private Camera thisCamera;
+    private AudioListener thisListener;
+    private Camera newCamera;
+    private AudioListener newListener;
 
     private void Start()
     {
         newPosition = newPositionTarget.position;
+        thisCamera = thisCameraObj.GetComponent<Camera>();
+        thisListener = thisCameraObj.GetComponent<AudioListener>();
+        newCamera = newCameraObj.GetComponent<Camera>();
+        newListener = newCameraObj.GetComponent<AudioListener>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,7 +27,9 @@ public class DoorCameraController : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             thisCamera.enabled = false;
+            thisListener.enabled = false;
             newCamera.enabled = true;
+            newListener.enabled = true;
             Transform player = collision.gameObject.GetComponent<Transform>();
             player.SetPositionAndRotation(newPosition, player.rotation);
         }
